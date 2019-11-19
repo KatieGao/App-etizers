73e675dd04ae732650fafa7cf9b470a0d8cb6d2c
mkperlmutter <mkperlmu@usc.edu>
2019-11-18 18:20:27 -0800

    Added user table

--- databasecode.sql
+++ databasecode.sql
@@ -1,3 +1,11 @@
 //creating the database
 CREATE DATABASE 201project;
 USC 201project;
+
+CREATE TABLE users
+(
+  user_id INT(11) NOT NULL,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  PRIMARY KEY (user_id)
+);
