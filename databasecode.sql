2bed9e513423eff612709c1f5744012e056614f9
yumign <35503192+yumign@users.noreply.github.com>
2019-11-18 18:43:05 -0800

    Update databasecode.sql

--- databasecode.sql
+++ databasecode.sql
@@ -9,3 +9,24 @@ CREATE TABLE users
   password VARCHAR(255) NOT NULL,
   PRIMARY KEY (user_id)
 );
+//creating the dishes_ingredients table
+CREATE TABLE dishes_ingredients(
+    index int(11) PRIMARY KEY 
+    FOREIGN KEY (dish_id) REFERENCES dishes(dish_id)
+    FOREIGN KEY (ingredient_id) REFERENCES ingredients(ingredient_id)
+);
+
+//creating the dishes table
+CREATE TABLE dishes(
+    dish_id int(11) PRIMARY KEY
+    FOREIGN KEY (restaurant_id) REFERENCES restaurants(restaurant_id)
+    price double(5,2)
+    dish_name varchar(255)
+    dish_thumbnail varchar(255)    
+);
+
+//creating the ingredients table
+CREATE TABLE dishes_ingredients(
+   ingredient_name varchar(255)
+    FOREIGN KEY (ingredient_id) REFERENCES dishes_ingredients(ingredient_id)
+);
