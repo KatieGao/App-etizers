955b010eb2669b5eef627739f4b51286809d6a9c
xizhem <45410768+xizhem@users.noreply.github.com>
2019-11-25 18:49:45 -0800

    Update databasecode.sql

--- databasecode.sql
+++ databasecode.sql
@@ -1,32 +1,71 @@
+
 //creating the database
 CREATE DATABASE 201project;
-USC 201project;
+USE 201project;
 
-CREATE TABLE users
-(
-  user_id INT(11) NOT NULL,
-  username VARCHAR(255) NOT NULL,
+CREATE TABLE users (
+  user_id INT(11) PRIMARY KEY,
+  username VARCHAR(255) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL,
-  PRIMARY KEY (user_id)
 );
-//creating the dishes_ingredients table
+
+CREATE TABLE users_favorite
+(
+  favorite_id INT(11) PRIMARY KEY,
+ 	dish_id INT(11) NOT NULL,
+  user_id INT(11) NOT NULL,
+  FOREIGN KEY (dish_id) REFERENCES dishes(dish_id),
+	FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE dishes
+(
+	dish_id INT(11) PRIMARY KEY,
+	restaurant_id INT(11) NOT NULL,
+	price DOUBLE(5, 2) NOT NULL,
+	dish_name VARCHAR(255) NOT NULL,
+	dish_thumbnail TEXT NOT NULL,
+	FOREIGN KEY (restaurant_id) REFERENCES restaurants(restaurant_id)
+);
+
+CREATE TABLE restaurants (
+Restaurant_id: int(11) PRIMARY KEY,
+Restaurant_name: VARCHAR(255) NOT NULL  UNIQUE,
+Phone: Varchar(10) UNIQUE,
+Address: Varchar(55) NOT NULL UNIQUE,
+Rating: Double(2,1),
+Genre_id: int(11) NOT NULL,
+restaurant_thumbnail: TEXT NOT NULL,
+FOREIGN KEY (genre_id) REFERENCES Genres(genre_id)
+
+
+
+CREATE TABLE reviews
+(
+review_id INT(11) PRIMARY KEY,
+dish_id INT(11) NOT NULL,
+content TEXT NOT NULL, 
+time_stamp DATETIME NOT NULL,
+FOREIGN KEY (dish_id) REFERENCES (dish_id)
+)  
+
+CREATE TABLE genres
+(
+genre_id INT(11) PRIMARY KEY NOT NULL,
+genre_name VARCHAR(255) NOT NULL,
+)
+
 CREATE TABLE dishes_ingredients(
-    index int(11) PRIMARY KEY 
-    FOREIGN KEY (dish_id) REFERENCES dishes(dish_id)
+    index INT(11) PRIMARY KEY,
+    dish_id INT(11) NOT NULL,
+    ingredient_id INT(11) NOT NULL,
+    FOREIGN KEY (dish_id) REFERENCES dishes(dish_id),
     FOREIGN KEY (ingredient_id) REFERENCES ingredients(ingredient_id)
 );
-
-//creating the dishes table
-CREATE TABLE dishes(
-    dish_id int(11) PRIMARY KEY
-    FOREIGN KEY (restaurant_id) REFERENCES restaurants(restaurant_id)
-    price double(5,2)
-    dish_name varchar(255)
-    dish_thumbnail varchar(255)    
-);
-
-//creating the ingredients table
+ 
 CREATE TABLE ingredients(
-   ingredient_name varchar(255)
-    FOREIGN KEY (ingredient_id) REFERENCES dishes_ingredients(ingredient_id)
+   ingredient_id INT(11) PRIMARY KEY,
+   ingredient_name varchar(255),
 );
+
+  
