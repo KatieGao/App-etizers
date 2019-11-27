e24e806a9b976058fe1736f111c831727030ddad
SONGRENHE <55167937+SONGRENHE@users.noreply.github.com>
2019-11-27 00:17:47 -0800

    Update databasecode.sql

--- databasecode.sql
+++ databasecode.sql
@@ -34,15 +34,6 @@ INSERT INTO restaurants (restaurant_name,genre_id)
          ('HABIT BURGERS',4),
 	 ('PANDA EXPRESS',1);
 
-//creating the dishes_ingredients table
-CREATE TABLE dishes_ingredients(
-    index int(11) PRIMARY KEY AUTO_INCREMENT, 
-    dish_id int(11),
-    FOREIGN KEY (dish_id) REFERENCES dishes(dish_id)
-    
-);
-
-
 //creating the dishes table
 CREATE TABLE dishes(
     dish_id int(11) PRIMARY KEY AUTO_INCREMENT,
@@ -92,7 +83,6 @@ INSERT INTO dishes (dish_name,price,restaurant_id)
 CREATE TABLE ingredients(
     ingredient_name varchar(255),
     ingredient_id int(11) PRIMARY KEY AUTO_INCREMENT,
-    FOREIGN KEY (ingredient_id) REFERENCES dishes_ingredients(ingredient_id)
 );
 INSERT INTO ingredients (ingredient_name)
   VALUES ('soup'),
@@ -109,7 +99,6 @@ INSERT INTO ingredients (ingredient_name)
          ('healthy'),
          ('vegetarian'),
          ('beans'),
-         ('corn'),
 	 ('chicken'),
 	 ('cabbage'),
 	 ('avocado'),
@@ -153,3 +142,80 @@ INSERT INTO ingredients (ingredient_name)
 	 ('egg'),
 	 ('roll');
      
+//creating the dishes_ingredients table
+CREATE TABLE dishes_ingredients(
+    index int(11) PRIMARY KEY AUTO_INCREMENT, 
+    dish_id int(11),
+    FOREIGN KEY (dish_id) REFERENCES dishes(dish_id),
+    ingredient_id int(11),
+    FOREIGN KEY (ingredient_id) REFERENCES dishes_ingredients(ingredient_id)
+    
+);
+//('soup'1),('salad'2),('pasta'3),('pizza'4),('bowl'5),
+//('burger'6),('sandwich'7),('tomatoes'8),('corn'9),('chillies'10),
+//('vegan'11),('healthy'12),('vegetarian'13),('beans'14),('chicken'15),
+//('cabbage'16),('avocado'17),('pepper'18),('noodles'19),('chinese'20),
+//('shrimp'21),('seafood'22),('meat sauce'23),('BBQ Sauce'24),('cheese'25),
+//('mushroom'26),('pepperoni'27),('sausage'28),('ham'29),
+INSERT INTO dishes_ingredients (dish_id, ingredient_id)
+  VALUES (1,1),
+  	(1,8),
+	(1,9),
+	(1,10),
+	(1,11),
+	(1,12),
+	(1,13),
+	(2,2),
+	(2,9),
+	(2,14),
+	(2,15),
+	(3,2),
+	(3,8),
+	(3,9),
+	(3,14),
+	(3,15),
+	(3,16),
+	(3,17),
+	(4,3),
+	(4,15),
+	(4,18),
+	(4,19),
+	(5,3),
+	(5,15),
+	(5,18),
+	(5,19),
+	(6,3),
+	(6,10),
+	(6,19),
+	(6,20),
+	(7,3),
+	(7,10),
+	(7,15),
+	(7,19),
+	(7,20),
+	(8,3),
+	(8,10),
+	(8,19),
+	(8,20),
+	(8,21),
+	(8,22),
+	(9,3),
+	(9,19),
+	(9,23),
+	(10,4),
+	(10,15),
+	(10,24),
+	(11,4),
+	(11,8),
+	(11,25),
+	(12,4),
+	(12,26),
+	(12,27),
+	(12,28),
+	(13,4),
+	(13,27),
+	(14,4),
+	(14,27),
+	(14,28),
+	(14,29);
+
