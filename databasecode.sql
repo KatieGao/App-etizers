03fa8a2071643bb9b91e55b7dac551f268ee3f10
yumign <35503192+yumign@users.noreply.github.com>
2019-12-01 19:24:04 -0800

    Update databasecode.sql

--- databasecode.sql
+++ databasecode.sql
@@ -4,7 +4,7 @@ USC 201project;
 
 CREATE TABLE users
 (
-  user_id INT(11) NOT NULL,
+  user_id INT(11) NOT NULL AUTO_INCREMENT,
   username VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   PRIMARY KEY (user_id)
